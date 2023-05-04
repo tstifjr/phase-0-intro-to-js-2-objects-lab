3e3d97effa4ef1ecc8f173c445b5e61700a4186a
TJ Stifter <tjstifter@gmail.com>
2023-05-04 16:10:47 -0400

    passes test

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Bill",
+    streetAddress: "123 N Lane",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = {...employee};
+    console.log(updatedEmployee);
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+    const copyEmployee = {...employee};
+    delete copyEmployee[key];
+    return copyEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    delete employee[key];
+    return employee;
+}
+;
